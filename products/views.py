0971eda39e73098dce2857dbb91735feba674cab
DiegoRmsR <diegoramos.mt@gmail.com>
2024-07-16 14:19:59 -0500

    Create product model to save de products

---
+++ products/views.py
@@ -0,0 +1,20 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import ProductForm
+from .models import Product
+
+# Create your views here.
+
+class ProductFormView(generic.FormView):
+    template_name = "products/add_product.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('add_product')
+    
+    def form_valid(slef, form):
+        form.save()
+        return super().form_valid(form)
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
